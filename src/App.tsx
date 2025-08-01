19f7baec1d6fe846638be99c1298caa2b27be362
ibrahimsuman <ibrahimsuman98@gmail.com>
2025-08-01 12:05:10 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { AlertDialogDemo } from './components/module/dialog'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+     <Button variant='outline'>Click me </Button>
+     <AlertDialogDemo></AlertDialogDemo>
+    </>
+  )
+}
+
+export default App
